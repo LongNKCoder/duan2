1fd1896dedd16a764f057e5f0a760229f2773bdb
Long Nguyen <longnguyen@hhdgroup.com>
2019-11-25 09:19:03 +0700

    xóa file main

--- chotot/main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import View,ListView
-from post import models
-
-class IndexView(ListView):
-    model = models.Category
-    context_object_name = 'category'
-    template_name = 'main/trangchu.html'
